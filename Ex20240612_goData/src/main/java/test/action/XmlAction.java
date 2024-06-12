19c004684e0cc2ed4ee4fdf47eeb7db4f48766a7
wjsxorjs <wjsxorjs@naver.com>
2024-06-12 10:53:24 +0900

    JDOM 20240612 0.2

--- Ex20240612_goData/src/main/java/test/action/XmlAction.java
+++ Ex20240612_goData/src/main/java/test/action/XmlAction.java
@@ -2,6 +2,7 @@ package test.action;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
+
+import test.vo.PersonalVO;
 
 public class XmlAction implements Action {
 
@@ -34,8 +37,34 @@ public class XmlAction implements Action {
 			
 			// 생성된 문서객체로부터 루트를 얻어내자
 			Element root = doc.getRootElement();
-			System.out.println(root.getName());
+//			System.out.println(root.getName());
 			
+			Element totalCount = root.getChild("totalCount");
+			List<Element> p_list = root.getChildren("personal");
+			// root.getChildren(); // 자식 모두(personal, totalCount)
+			
+			PersonalVO[] p_ar = new PersonalVO[p_list.size()];
+			// 위의 배열이 지금은 비어있지만 아래의 반복문을 수행하면
+			// 채워지도록 해야하고 이 배열이 request에 저장되어야한다.
+			int idx = 0;
+			
+			for(Element personal: p_list) {
+				// List<Element> children = e.getChildren();
+				// Element name = e.getChild("name");
+				// 이름 - personal안에 있는 자식들 중
+				//		 태그이름이 "name"인 요소의 문자열 가져오기
+				String name = personal.getChildText("name");
+				String email = personal.getChildText("email");
+				
+//				System.out.printf("이름: %s || 이메일: %s\r\n",name,email);
+				
+				PersonalVO pvo = new PersonalVO(name, email);
+				
+				p_ar[idx] = pvo;
+				idx++;
+			} // for의 끝
+			
+			request.setAttribute("p_ar", p_ar);
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -43,7 +72,7 @@ public class XmlAction implements Action {
 		}
 		
 		
-		return null;
+		return "/jsp/personal.jsp";
 	}
 
 }
